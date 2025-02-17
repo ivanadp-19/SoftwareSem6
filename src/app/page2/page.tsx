4f1a49cc75e9b318d3a23f902296ce912a607dff
ivanadp-19 <69006652+ivanadp-19@users.noreply.github.com>
2025-02-17 12:01:20 -0600

    tercera pagina

---
+++ src/app/page2/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+
+export default function Construccion() {
+ 
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen ">
+      <h2 className="text-2xl font-semibold mb-3">En Construcción</h2>
+    </div>
+  )
+}2
